8f463722111eed7e96868921b95f4ee77d47b6b8
petekozz <66498808+petekozz@users.noreply.github.com>
2020-10-28 20:26:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const div = document.querySelector('#container');
+    div.style.textAlign = 'center';
+    
+const image = document.querySelector('img');
+    image.style.width = '150px';
+    image.style.borderRadius = '50%';
